5f6cbf0d4fb602a4be1c6e5e36086a10fbe35d31
Oscar <balobba@gmail.com>
2020-02-22 11:18:59 +0100

    Add initial promille screen and IOS adjustments

--- screens/PromilleScreen.js
+++ screens/PromilleScreen.js
@@ -1,12 +1,76 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import Header from "../components/Header";
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Picker,
+  Platform
+} from 'react-native';
+import Header from '../components/Header';
 
 const Promille = ({ navigation }) => {
+  const [ethanol, setEthanol] = useState();
+  const [sampletype, setSampletype] = useState();
+  const [unit, setUnit] = useState();
+
+  const calculateAlcoholLevel = () => {
+    let res = 0;
+    //Math.round((20 * parseInt(ethanol) * 1000) / 60 / parseInt(time));
+
+    if (sampletype == 'Blood') {
+      res = (ethanol * 0.046) / 1.055;
+    } else {
+      res = ((ethanol / 1.16) * 0.046) / 1.055 - 0.00163;
+    }
+
+    if (!res || res == Infinity || ethanol == Infinity || !ethanol) {
+      return '';
+    } else {
+      return res.toFixed(3) + '‰';
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header navigation={navigation} />
-      <Text>Promille.js</Text>
+
+      <Text>Etanol (i mg etanol per g helblod):</Text>
+      <TextInput
+        keyboardType="numeric"
+        maxLength={10}
+        style={styles.ethanolField}
+        onChangeText={ethanol => setEthanol(ethanol.replace(/[^0-9]/g, ''))}
+        ethanol={ethanol}
+      />
+
+      <Picker
+        selectedValue={sampletype}
+        style={styles.picker}
+        onValueChange={sampleValue => {
+          setSampletype(sampleValue);
+        }}
+      >
+        <Picker.Item label="Serum" value="Serum" />
+        <Picker.Item label="Plasma" value="Plasma" />
+        <Picker.Item label="Blood" value="Blood" />
+      </Picker>
+
+      <Picker
+        selectedValue={unit}
+        style={styles.picker}
+        onValueChange={unitValue => {
+          setUnit(unitValue);
+        }}
+      >
+        <Picker.Item label="mmol/L" value="mmol/L" />
+        <Picker.Item label="mg/ml" value="mg/ml" />
+      </Picker>
+
+      <View style={styles.resultView}>
+        <Text>Klinisk: {calculateAlcoholLevel()}</Text>
+        <Text>Rättslig: {calculateAlcoholLevel()}</Text>
+      </View>
     </View>
   );
 };
@@ -14,9 +78,18 @@ const Promille = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  ethanolField: { height: 40, borderColor: 'gray', borderWidth: 1, width: 200 },
+  picker: {
+    height: 50,
+    width: 150,
+    marginBottom: Platform.OS === 'ios' ? 100 : 0
+  },
+  resultView: {
+    marginTop: Platform.OS === 'ios' ? 10 : 0
   }
 });
 
