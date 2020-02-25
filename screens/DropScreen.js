dfba45afc15f5a91a7e99b0029b2d5522e70701c
Oscar <balobba@gmail.com>
2020-02-25 21:46:50 +0100

    Add styling to dropscreen

--- screens/DropScreen.js
+++ screens/DropScreen.js
@@ -20,7 +20,7 @@ const DropScreen = ({ navigation }) => {
     ) {
       return '';
     } else {
-      return res;
+      return res.toString() + ' droppar per milliliter';
     }
   };
 
@@ -31,27 +31,29 @@ const DropScreen = ({ navigation }) => {
       scrollEnabled={false}
     >
       <Header navigation={navigation} />
+      <View style={styles.inputContainer}>
+        <Text style={styles.fieldText}>Mängd (L):</Text>
+        <TextInput
+          keyboardType="numeric"
+          maxLength={10}
+          placeholder='Skriv ett värde, exempelvis "25"'
+          style={styles.amountField}
+          onChangeText={amount => setAmount(amount.replace(/[^0-9]/g, ''))}
+          amount={amount}
+        />
 
-      <Text>Mängd (L):</Text>
-      <TextInput
-        keyboardType="numeric"
-        maxLength={10}
-        style={styles.amountField}
-        onChangeText={amount => setAmount(amount.replace(/[^0-9]/g, ''))}
-        amount={amount}
-      />
-
-      <Text>Tid (h):</Text>
-      <TextInput
-        keyboardType="numeric"
-        maxLength={10}
-        style={styles.amountField}
-        onChangeText={time => setTime(time.replace(/[^0-9]/g, ''))}
-        time={time}
-      />
-
-      <View>
-        <Text>{calculateDropValue()}</Text>
+        <Text style={styles.fieldText}>Tid (h):</Text>
+        <TextInput
+          keyboardType="numeric"
+          maxLength={10}
+          placeholder='Skriv ett värde, exempelvis "25"'
+          style={styles.amountField}
+          onChangeText={time => setTime(time.replace(/[^0-9]/g, ''))}
+          time={time}
+        />
+      </View>
+      <View style={styles.resContainer}>
+        <Text style={styles.resNumberText}>{calculateDropValue()}</Text>
       </View>
     </ScrollView>
   );
@@ -64,7 +66,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  amountField: { height: 40, borderColor: 'gray', borderWidth: 1, width: 200 }
+  amountField: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 250,
+    textAlign: 'center',
+    marginTop: 10
+  },
+  fieldText: {
+    textAlign: 'center',
+    fontSize: 20,
+    padding: 4
+  },
+  inputContainer: {
+    paddingTop: 100,
+    flex: 3
+  },
+  resContainer: {
+    flex: 3,
+    alignItems: 'center'
+  },
+  resNumberText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ff5252'
+  }
 });
 
 export default DropScreen;
