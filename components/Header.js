7941bb325b292855d4a0b0ccbbc2a1d9b5dc44de
Oscar <balobba@gmail.com>
2020-04-26 17:56:49 +0200

    Add dynamic custom header background

--- components/Header.js
+++ components/Header.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Header = ({ navigation }) => {
+const Header = ({ navigation, headerColor, burgerColor, headerTextColor }) => {
   const openMenu = () => {
     // Open drawer with the navigation prop
     navigation.openDrawer();
   };
 
   return (
-    <View style={styles.header}>
+    <View style={{ ...styles.header, backgroundColor: headerColor }}>
       <MaterialIcons
         name="menu"
         size={36}
-        style={styles.burger}
+        style={{ ...styles.burger, color: burgerColor }}
         onPress={openMenu}
       />
-      <Text style={styles.headerTitle}>{navigation.state.routeName}</Text>
+      <Text style={{ ...styles.headerTitle, color: headerTextColor }}>
+        {navigation.state.routeName}
+      </Text>
     </View>
   );
 };
@@ -28,16 +30,19 @@ const styles = StyleSheet.create({
     width: '100%',
     left: 0,
     top: 0,
-    position: 'absolute'
+    position: 'absolute',
+    backgroundColor: '#ffffff',
   },
   burger: {
     position: 'absolute',
     left: 8,
-    top: 8
+    top: 8,
+    color: '#000000',
   },
   headerTitle: {
     alignSelf: 'center',
     fontSize: 24,
-    marginTop: 10
-  }
+    marginTop: 10,
+    color: '#ffffff',
+  },
 });
