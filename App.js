46ee0d97457595569cf2e858d5453ca00a2725e3
Oscar <balobba@gmail.com>
2020-04-27 17:19:08 +0200

    Bugfix where navbar was behind 'notch' on newer phone models

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import Navigator from './navigation/drawer.js';
+import { SafeAreaView } from 'react-navigation';
 StatusBar.setHidden(true);
 export default function App() {
-  return <Navigator />;
+  return (
+    <SafeAreaView style={styles.app}>
+      <Navigator />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+  },
+});
